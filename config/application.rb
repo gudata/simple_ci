474612e5f6c47a419f88560888ee44edfd0201eb
gudata <i.bardarov@gmail.com>
2014-03-04 18:14:08 +0200

    fonts

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module SimpleCi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    config.assets.precompile += [
+      'fonts/foundation-icons.eot',
+      'fonts/foundation-icons.svg',
+      'fonts/foundation-icons.ttf',
+      'fonts/foundation-icons.woff',
+    ]
   end
 end
