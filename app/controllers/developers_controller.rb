b142c5d10d26d586044534339994dad23d44a941
gudata <i.bardarov@gmail.com>
2014-03-12 11:42:19 +0200

    fix password change

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,5 @@
 class DevelopersController < InheritedResources::Base
   def permitted_params
-    params.permit(developer: [:password, :can_login])
+    params.permit(developer: [:password, :password_confirmation, :can_login])
   end
 end
