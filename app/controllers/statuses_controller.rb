d7b264a6302f779d84e55b61e009a5f06b289850
gudata <i.bardarov@gmail.com>
2014-03-20 14:04:16 +0200

    allow iframes

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -8,5 +8,6 @@ class StatusesController < ApplicationController
   def show
     @state_behaviour = StateBehaviour.new(params[:token])
     @branch = @state_behaviour.branch
+    response.headers.except! 'X-Frame-Options'
   end
 end
