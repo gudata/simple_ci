596c0276ac4b2f010bd90d34d7ad5c01fedaeb26
gudata <i.bardarov@gmail.com>
2014-03-20 14:05:30 +0200

    missing end

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -70,9 +70,11 @@ class Runner
       ActiveRecord::Base.connection_pool.disconnect!
       ActiveSupport.on_load(:active_record) do
         config = ActiveRecord::Base.configurations[Rails.env]
-      config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-      config['pool']              = ENV['DB_POOL']      || 5
-      ActiveRecord::Base.establish_connection(config)
+        config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+        config['pool']              = ENV['DB_POOL']      || 5
+        ActiveRecord::Base.establish_connection(config)
+      end
     end
   end
+
 end
